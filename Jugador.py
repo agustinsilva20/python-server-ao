8cd3987cd50a92b3d647d4a301515a727024c237
agussilva20 <66189054+agussilva20@users.noreply.github.com>
2023-01-30 17:03:00 -0300

    add documentation to code

--- Jugador.py
+++ Jugador.py
@@ -7,18 +7,20 @@ class Jugador:
         self.game_controller = game_controller
 
     def recibir_comandos(self):
+        print("Jugador nuevo: Escuchando datos")
         while True:
-            # recibir el tamaño del paquete
+            # Recibe los primeros 4 bytes del mensaje en el socket
             tamaño_paquete = self.socket.recv(4)
             if not tamaño_paquete:
                 break
-            tamaño_paquete = struct.unpack("!I", tamaño_paquete)[0]
+            tamaño_paquete = struct.unpack("!I", tamaño_paquete)[0] # Desempaqueta los 4 bytes. se usa para determinar cuantos bytes debe recibir la funcion "recv()"
 
             # recibir el paquete completo
             paquete = b""
             while len(paquete) < tamaño_paquete:
                 paquete += self.socket.recv(tamaño_paquete - len(paquete))
 
+            print("Paquete recivido: " + str(paquete))
             self.game_controller.agregar_comando(self, paquete)
 
     def enviar_datos(self, data):
--- ManejarConexionUsuario.py
+++ ManejarConexionUsuario.py
@@ -1,9 +1,9 @@
-import Jugador
+from Jugador import Jugador
 
 def handle_client(client_socket, client_address, game_controller):
     # Recibe y maneja comandos del jugador
-    # Instancia una clase de Jugador
-    player = Jugador(client_socket, client_address, game_controller)
+    player = Jugador(client_socket, client_address, game_controller) # Instancia una clase de Jugador
+    print("Instancia de jugador creada en un nuevo hilo")
 
     # Lo agrega a Game Controler
     game_controller.agregar_jugador(player)
